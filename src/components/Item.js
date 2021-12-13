48ab99bc70aaee30c3e03a0ce1d7e6dc6fd467f6
Casey Harding <casey.r.harding@gmail.com>
2021-12-13 13:34:28 -0500

    Final solution code in place.

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,38 +1,64 @@
 import React from 'react'
 // We'll need quite a few imports from react-router-dom
-import { Route, NavLink, Switch, useParams, useRouteMatch } from 'react-router-dom';
+import { 
+  Route, 
+  NavLink, 
+  Switch, 
+  useParams, 
+  useRouteMatch, 
+  useHistory
+} from 'react-router-dom';
 import ItemDetails from './ItemDetails'
 
 export default function Item(props) {
   // We get ALL items through props. We'll use the URL to find out which item is the one to show.
   const { items } = props
+  const history = useHistory();
 
   // 👉 STEP 7 - We need to pull item from items, using a parameter in the URL (:itemID)
   // Beware! The ids are integers, whereas URL parameters are strings.
   // Beware! The JSX is expecting 'item' to exist instantly!
   // we use this hook to grab they dynamic parts of the path (:itemID).
-  const item = {}
+  const { itemID } = useParams();
+  const { url, path } = useRouteMatch();
+
+  // NEVER EVER EVER EVER USE DOUBLE EQUALS
+  const item = items.find(elem => elem.id === parseInt(itemID))
+
+  // if (!item) {
+  //   history.push('/');
+  // }
 
   return (
-    <div className='item-wrapper'>
-      <div className='item-header'>
-        <div className='image-wrapper'>
-          <img src={item.imageUrl} alt={item.name} />
+      <div className='item-wrapper'>
+        <div className='item-header'>
+          <div className='image-wrapper'>
+            <img src={item.imageUrl} alt={item.name} />
+          </div>
+          <div className='item-title-wrapper'>
+            <h2>{item.name}</h2>
+            <h4>${item.price}</h4>
+          </div>
         </div>
-        <div className='item-title-wrapper'>
-          <h2>{item.name}</h2>
-          <h4>${item.price}</h4>
-        </div>
+
+        <nav className='item-sub-nav'>
+          {/* 👉 STEP 8 - Here go the NavLinks to `<current url>/shipping` and `<current url>/description` */}
+          <NavLink to={`${url}/shipping`}>Shipping</NavLink>
+          <NavLink to={`${url}/description`}>Description</NavLink>
+        </nav>
+
+        {/* 👉 STEP 9 - Here go the Routes for `<current path>/shipping` and `<current path>/description` */}
+        {/* These Routes should render <ItemDetails /> */}
+
+        {/* 👉 STEP 10 - Shorten paths and urls with `useRouteMatch` hook */}
+        <Switch>
+          <Route path={`${path}/shipping`}>
+            <ItemDetails text={item.shipping} />
+          </Route>
+          <Route path={`${path}/description`}>
+            <ItemDetails text={item.description} />
+          </Route>
+        </Switch>
       </div>
-
-      <nav className='item-sub-nav'>
-        {/* 👉 STEP 8 - Here go the NavLinks to `<current url>/shipping` and `<current url>/description` */}
-      </nav>
-
-      {/* 👉 STEP 9 - Here go the Routes for `<current path>/shipping` and `<current path>/description` */}
-      {/* These Routes should render <ItemDetails /> */}
-
-      {/* 👉 STEP 10 - Shorten paths and urls with `useRouteMatch` hook */}
-    </div>
   )
 }

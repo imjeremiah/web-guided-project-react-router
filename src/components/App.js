ae9c8e8a209b526de2a7c6de07f959f3e0d9b89d
Casey Harding <casey.r.harding@gmail.com>
2021-12-13 13:04:19 -0500

    Through to ItemsList.

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 // 👉 STEP 2 - React Router imports (Route, Link and Switch)
-import { Link } from 'react-router-dom';
+import { Link, Route, Switch } from 'react-router-dom';
 
 // Components used for the different routes
-import Home from './Home'
-import ItemsList from './ItemsList'
-import Item from './Item'
+import Home from './Home';
+import ItemsList from './ItemsList';
+import Item from './Item';
 
 // Dummy data
-import data from '../data'
+import data from '../data';
 
 function fetchStock() {
   // fetchStock simulates getting data through axios.get(<URL>)
-  return Promise.resolve({ success: true, data })
+  return Promise.resolve({ success: true, data });
 }
 
 export default function App(props) {
-  const [stock, setStock] = useState([])
+  const [stock, setStock] = useState([]);
 
   useEffect(() => {
     fetchStock().then(res => setStock(res.data))
@@ -34,6 +34,24 @@ export default function App(props) {
       </nav>
 
       {/* 👉 STEP 4 - Build a Switch with a Route for each of the components imported at the top */}
+      <Switch>
+        <Route path="/items-list/:itemID">
+          <Item items={stock} />
+        </Route>
+        <Route path="/items-list">
+          <ItemsList items={stock} />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+
+      {/**
+       * 
+       * <Route path="/items-list" render={(props) => (
+       *   <ItemsList items={stock} />
+       * )} />
+       */}
     </div>
   )
 }
